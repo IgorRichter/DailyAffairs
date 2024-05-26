f06dbdcfb48737e8bc24b4516625ae72b018674b
Igor Richter <i.rixter201572@gmail.com>
2024-05-26 15:18:18 +0300

    fix some error

--- source/scripts/main.js
+++ source/scripts/main.js
@@ -5,16 +5,3 @@ import './data-sort.js';
 import './sort.js';
 import './task-types-sort.js';
 import './project-section.js';
-
-var video = document.getElementById("myVideo");
-var playPauseButton = document.getElementById("playPause");
-
-playPauseButton.addEventListener("click", function() {
-    if (video.paused) {
-        video.play();
-        playPauseButton.textContent = "Pause";
-    } else {
-        video.pause();
-        playPauseButton.textContent = "Play";
-    }
-});
